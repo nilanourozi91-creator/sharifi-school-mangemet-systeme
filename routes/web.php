5e191b7615e058f7cee831284f6b95a152a6c246
nila nourozi <nilanourozi91@gmail.com>
2026-04-17 06:56:15 +0430

    some route added

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
+route::prefix('dashboard')->middleware('auth','verified')->group(function () {
+    route::view('department.⚡index');
+    route::view('payment.⚡index');
+    route::view('salary.⚡index');
+    route::view('employee.⚡index');
+    route::view('descnation.⚡index');
+    route::view('contract.⚡index');
+    route::view('school.⚡index');
+    route::view('payroll.⚡index');
+});
 require __DIR__.'/auth.php';
