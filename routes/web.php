ebc03057ab1980e9f11c02cde6c2312834c10fa7
nila nourozi <nilanourozi91@gmail.com>
2026-04-18 07:03:48 +0430

    code updated

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 route::prefix('dashboard')->middleware('auth','verified')->group(function () {
-    route::view('department.⚡index');
-    route::view('payment.⚡index');
-    route::view('salary.⚡index');
-    route::view('employee.⚡index');
-    route::view('descnation.⚡index');
-    route::view('contract.⚡index');
-    route::view('school.⚡index');
-    route::view('payroll.⚡index');
+    route::view('department','department.⚡edite');
+    route::view('school','school.⚡index');
+    route::view('contract','contract.⚡index');
+    route::view('payroll','payroll.⚡index');
+    route::view('payment','payment.⚡index');
 });
 require __DIR__.'/auth.php';
